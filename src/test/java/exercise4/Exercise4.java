090e4dd663a727cf183cf9b5e52296c47d16e263
IradVS <jose.iradier.vs@gmail.com>
2022-03-17 02:44:47 -0600

    Exercise test 2 added

--- src/test/java/exercise4/Exercise4.java
+++ src/test/java/exercise4/Exercise4.java
@@ -34,33 +34,30 @@ public class Exercise4 {
 	public void tc_REQ005_001() throws InterruptedException {
 		driver.get("https://practice.automationbro.com/my-account/");
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-		Actions action=new Actions(driver);
-		WebElement shopMenu = driver.findElement(By.xpath("//*[@id='menu-item-567']/a"));
+		Actions action = new Actions(driver);
+		// login
 		WebElement userInput = driver.findElement(By.xpath("//*[@id='username']"));
 		WebElement passwordInput = driver.findElement(By.xpath("//*[@id='password']"));
 		WebElement loginBtn = driver.findElement(By.xpath("//*[@id='customer_login']/div[1]/form/p[3]/button"));
 		userInput.sendKeys(user);
 		passwordInput.sendKeys(password);
 		loginBtn.click();
-		
+		// go to shop
 		Thread.sleep(5000);
-		shopMenu = driver.findElement(By.xpath("//*[@id='menu-item-567']/a"));
+		WebElement shopMenu = driver.findElement(By.xpath("//*[@id='menu-item-567']/a"));
 		shopMenu.click();
-		
+		// open a product
 		int products = driver.findElements(By.xpath("//*[@id='primary']/ul//li")).size();
 		int getProduct = (int) (Math.random() * products + 1);
 		System.out.println(getProduct);
-		// WebElement
-		// addBtn=driver.findElement(By.xpath("//*[@id='primary']/ul//li["+getProduct+"]//a[text()='Add
-		// to cart']"));
 		WebElement product = driver.findElement(By.xpath("//*[@id='primary']/ul//li[" + getProduct + "]/a/h2"));
 		String productSelected = product.getText();
 		product.click();
-		
+		// return to shop
 		Thread.sleep(5000);
 		shopMenu = driver.findElement(By.xpath("//*[@id='menu-item-567']/a"));
 		shopMenu.click();
-		
+		// check if the item is displayed in recent files
 		WebElement recentProduct = driver
 				.findElement(By.xpath("//*[@id='woocommerce_recently_viewed_products-1']/ul/li[1]/a/span"));
 		System.out.println("selected " + productSelected);
@@ -69,10 +66,34 @@ public class Exercise4 {
 		Assert.assertEquals(productSelected, recentProduct.getText());
 	}
 
+	@Test
+	public void tc_REQ007_001() {
+		driver.get("https://practice.automationbro.com/contact/");
+		//Enter data
+		WebElement nameField = driver.findElement(By.xpath("//*[@id='evf-277-field_ys0GeZISRs-1']"));
+		WebElement emailField = driver.findElement(By.xpath("//*[@id='evf-277-field_LbH5NxasXM-2']"));
+		WebElement phoneField = driver.findElement(By.xpath("//*[@id='evf-277-field_66FR384cge-3']"));
+		WebElement messageField = driver.findElement(By.xpath("//*[@id='evf-277-field_yhGx3FOwr2-4']"));
+		WebElement submitBtn = driver.findElement(By.xpath("//*[@id='evf-submit-277']"));
+		nameField.sendKeys("Jose");
+		emailField.sendKeys("jose@gmail.com");
+		phoneField.sendKeys("1234567891");
+		messageField.sendKeys("Hola mundo");
+		//click on submit
+		submitBtn.click();
+		//verify message
+		WebElement submitMessage=driver.findElement(By.xpath("//*[@id='primary']/div/div/div/section[3]/div/div/div/div/div/section[2]/div/div/div[2]/div/div/div/div/div/div/div"));
+		System.out.println(submitMessage.getText());
+		Assert.assertEquals(submitMessage.getText(), "Thanks for contacting us! We will be in touch with you shortly");
+	}
+
 	@AfterMethod
 	public void tearDown() throws InterruptedException {
 		System.out.println("EXIT");
 		Thread.sleep(5000);
 		driver.quit();
 	}
+	// WebElement
+	// addBtn=driver.findElement(By.xpath("//*[@id='primary']/ul//li["+getProduct+"]//a[text()='Add
+	// to cart']"));
 }
