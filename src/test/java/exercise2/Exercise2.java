5b90c7cae3a9fe18a1cf3cdace87d28e749f94ed
IradVS <jose.iradier.vs@gmail.com>
2022-03-18 13:22:37 -0600

    Removed unused lines

--- src/test/java/exercise2/Exercise2.java
+++ src/test/java/exercise2/Exercise2.java
@@ -1,6 +1,5 @@
 package exercise2;
 
-import java.time.Duration;
 import java.util.List;
 
 import org.openqa.selenium.By;
@@ -10,7 +9,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -37,7 +35,6 @@ public class Exercise2 {
 
 	@Test
 	public void suggestionClassTest() throws InterruptedException {
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3));
 		WebElement suggestionInput = driver.findElement(By.xpath("//input[@id='autocomplete']"));
 		suggestionInput.sendKeys("Mexi");
 		Thread.sleep(1000);
